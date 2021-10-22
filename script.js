ca5ebda57aac2329e974543c2c7939d7dbd2b758
Max Bagatini Alves <maxbagatini@hotmail.com>
2021-10-21 20:52:20 -0400

    Add projects.html + script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const nav_underline = document.querySelector('hr');
+
+if (window.location.pathname == "/index.html" || window.location.pathname == "/")
+{
+    nav_underline.style.marginLeft = "1.2%";
+    nav_underline.style.width = "18%";
+}
+else if (window.location.pathname == "/projects.html")
+{
+    nav_underline.style.marginLeft = "31%";
+    nav_underline.style.width = "24.5%";
+}
+else if (window.location.pathname == "/contact.html")
+{
+    nav_underline.style.marginLeft = "67.2%";
+    nav_underline.style.width = "23.8%";
+}
