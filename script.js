b20891e2c4c9cff98c9b79e23a0c448ea0fd62a3
Max Bagatini Alves <maxbagatini@hotmail.com>
2021-10-22 18:29:57 -0400

    Updated comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// gets the first <hr> element.
+// you can do .hr for an element that has "hr" class
+// or #hr for an element that has "hr" id
 const nav_underline = document.querySelector('hr');
 
 if (window.location.pathname == "/index.html" || window.location.pathname == "/")
